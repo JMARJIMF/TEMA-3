048780c47dd3acbe1ed714dfcf9a2b687a7617a2
JMARJIMF <163421508+JMARJIMF@users.noreply.github.com>
2024-03-14 21:36:58 +0100

    Add files via upload

---
+++ Actividad 2/js/script.js.js
@@ -0,0 +1,14 @@
+{function Saludar(){
+alert("hola")
+segundaFuncion()
+}
+function segundaFuncion() {
+alert(":-)")
+}
+function Despedirse() {
+alert("adios")
+}
+function segundaFuncion() {
+alert(":)")   
+}
+}
---
+++ Actividad 3/js (1)/script.js (1).js
@@ -0,0 +1,7 @@
+function pedirDatos(){
+let edad=prompt("¿Cuantos años tienes?");
+let nombre=prompt("¿Como te llamas?");
+alert ("hola " + nombre +" tienes " + edad + " años")
+
+
+}
